e325be784f1e214a3d6fba4fb403a40f6bcd70c5
unknown <miroenlared@gmail.com>
2022-09-18 10:55:33 -0500

    avance maquetacion

--- src/components/Wrapinfo.js
+++ src/components/Wrapinfo.js
@@ -1,4 +1,5 @@
 import React from "react";
+import iconCart from "../assets/icon-cart.svg"
 import img01 from "../assets/image-product-1.jpg"
 const WrapInfo = () => {
     return(
@@ -17,7 +18,7 @@ const WrapInfo = () => {
                     <p className="product-description">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                    Ut enim ad minim veniam, quis nostrud exercitation  ullamco laboris 
                     nisi ut aliquip ex ea commodo consequat.
                     </p>
 
@@ -27,6 +28,20 @@ const WrapInfo = () => {
                             <div className="product-discount">50%</div>
                         </div>
                         <div className="product-oldPrice">$250.00</div>
+                    </div>
+
+
+                    <div className="product-wrapBtn">
+                        <div className="product-wrapQuantity">
+                            <button className="product-minusQuantity">-</button>
+                            <div className="product-numQuantity">0</div>
+                            <button className="product-minusQuantity">+</button>
+
+                        </div>
+                        <button className="product-btn">
+                            <img src={iconCart} alt="Icon cart"/>
+                            <span>Add to cart</span>
+                        </button>
                     </div>
 
 
