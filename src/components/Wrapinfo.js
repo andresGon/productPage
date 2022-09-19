940b4d6542fe6789b449bf72404c88b7ecbc95d0
unknown <miroenlared@gmail.com>
2022-09-18 20:48:45 -0500

    avance de maqueta y logica

--- src/components/Wrapinfo.js
+++ src/components/Wrapinfo.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import iconCart from "../assets/icon-cart.svg"
 import img01 from "../assets/image-product-1.jpg"
+import img02 from "../assets/image-product-2.jpg"
+import img03 from "../assets/image-product-3.jpg"
+import img04 from "../assets/image-product-4.jpg"
+
+import img01Xs from "../assets/image-product-1-thumbnail.jpg"
+import img02Xs from "../assets/image-product-2-thumbnail.jpg"
+import img03Xs from "../assets/image-product-3-thumbnail.jpg"
+import img04Xs from "../assets/image-product-4-thumbnail.jpg"
+
 const WrapInfo = () => {
+    const [quantity, setQuantity] = useState(0);
+    const [image, setImage] = useState(0);
+
+    console.log(image)
     return(
         <>
             <div className="wrapInfo-wrap">
@@ -9,6 +22,21 @@ const WrapInfo = () => {
                     <figure className="product-wrapImg">
                         <img src={img01} />
                     </figure>
+
+                    <div className="product-rowThumbnail">
+                        <div className="product-thumbnail active" onClick={()=>setImage(0)}>
+                            <img src={img01Xs}/>
+                        </div>
+                        <div className="product-thumbnail" onClick={()=>setImage(1)}>
+                            <img src={img02Xs}/>
+                        </div>
+                        <div className="product-thumbnail" onClick={()=>setImage(2)}>
+                            <img src={img03Xs}/>
+                        </div>
+                        <div className="product-thumbnail" onClick={()=>setImage(3)}>
+                            <img src={img04Xs}/>
+                        </div>
+                    </div>
                 </div>
                 <div className="wrapInfo-col">
                     <div className="product-brand">Sneaker company</div>
@@ -33,9 +61,9 @@ const WrapInfo = () => {
 
                     <div className="product-wrapBtn">
                         <div className="product-wrapQuantity">
-                            <button className="product-minusQuantity">-</button>
-                            <div className="product-numQuantity">0</div>
-                            <button className="product-minusQuantity">+</button>
+                            <button className="product-minusQuantity" onClick={()=>setQuantity(quantity - 1)}>-</button>
+                            <div className="product-numQuantity">{quantity}</div>
+                            <button className="product-minusQuantity" onClick={()=>{setQuantity(quantity + 1)}}>+</button>
 
                         </div>
                         <button className="product-btn">
